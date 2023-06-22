d698daa71590865702fe8d565ab7978b9bcf882b
surendravss <surendra.ediga@vishleshan.net>
2023-06-22 21:39:56 +0530

    component & css

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './footer'
+
+
+ const Header = () => {
+  return (
+    <div style={{backgroundColor:'red',fontSize:120}}>
+      <h1>this is header</h1>
+    </div>
+  )
+} //arrow fun
+
+export default Header; 
---
+++ src/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import "./footer.css"
+function Footer() {
+  return (
+   <div className="main">
+    <h1>this is a footer</h1>
+   </div>
+  )
+}
+
+export default Footer;
